2c8c3eee813a44b9946dc8be12651d74882c34d3
LazuliKao <lazulikao@nit.zju.edu.cn>
2022-12-06 18:05:49 +0800

    sync

--- StudyVue/VueShop/src/main.ts
+++ StudyVue/VueShop/src/main.ts
@@ -1,6 +1,6 @@
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
